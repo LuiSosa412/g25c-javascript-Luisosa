0acbd57faed8cda4e9a30fc8534602b3dce5344b
LuiSosa412 <edgarsus519@gmail.com>
2023-05-17 13:09:15 -0500

    problemas para realizar depositos

--- 8-proyecto/programacion.js
+++ 8-proyecto/programacion.js
@@ -5,8 +5,11 @@ function login() {
     password = document.getElementById("contraseña").value;
 
     if( user == "prueba" && password == "1234"){
-        alert ("iniciaste secion")
+        // alert ("iniciaste secion")
+        window.location = "index2.html";
     } else {
         alert ("datos incorectos")
     }
 }
+
+
